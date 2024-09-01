f75400290fd28a1b4b1d33eff64cb8af04801c50
Carlos Nadal <kali@kali>
2024-08-31 22:26:59 -0400

    updated

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -7,12 +7,12 @@ and each box may contain keys to the other boxes
 """
 
 
-def canUnlockAll(boxes):
+def can_unlock_all(boxes):
     """
-    Determines if all boxes can be unlocked.
+    Check if all boxes can be unlocked.
 
     Args:
-        boxes (list of list of int): 
+        boxes (list of list of int):
         A list of boxes, where each box is represented
         by a list of keys it contains. Each key is an
         integer representing a box that can be unlocked.
@@ -24,7 +24,7 @@ def canUnlockAll(boxes):
     """
     # Start with the first box (box 0) unlocked
     unlocked = set([0])
-    
+
     # Use a stack to keep track of boxes to explore
     stack = [0]
 
@@ -33,10 +33,11 @@ def canUnlockAll(boxes):
         current_box = stack.pop()
         # Iterate through keys in the current box
         for key in boxes[current_box]:
-            # If the key unlocks a new box, add it to the unlocked set and stack
+            # If the key unlocks a new box,
+            # add it to the unlocked set and stack
             if key not in unlocked and key < len(boxes):
                 unlocked.add(key)
                 stack.append(key)
-                
+
     # Return True if all boxes are unlocked, otherwise False
     return len(unlocked) == len(boxes)
