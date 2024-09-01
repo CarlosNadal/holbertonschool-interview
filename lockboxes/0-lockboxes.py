279518f736291d905eba82c1680c4f6a9be0eb57
Carlos Nadal <kali@kali>
2024-08-31 22:08:59 -0400

    updated

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,20 +4,15 @@ def canUnlockAll(boxes):
     Determines if all boxes can be unlocked.
 
     Args:
-        boxes (list of list of int): A list of boxes, where each box is represented
-                                     by a list of keys it contains. Each key is an
-                                     integer representing a box that can be unlocked.
+        boxes (list of list of int): 
+        A list of boxes, where each box is represented
+        by a list of keys it contains. Each key is an
+        integer representing a box that can be unlocked.
 
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
 
-    Example:
-        >>> canUnlockAll([[1], [2], [3], [4], []])
-        True
-        >>> canUnlockAll([[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]])
-        True
-        >>> canUnlockAll([[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]])
-        False
+
     """
     # Start with the first box (box 0) unlocked
     unlocked = set([0])
